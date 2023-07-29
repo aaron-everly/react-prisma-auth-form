114afc813c3ec18b80c778a387c6abb0410b8e36
Aaron Everly <40549689+aaron-everly@users.noreply.github.com>
2023-07-29 00:56:19 -0700

    Fix linting for Home.jsx

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -13,7 +13,9 @@ export function Home({ user, setUser }) {
     return (
         <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
             <div className="mx-auto max-w-lg">
-                <h1 className="text-2xl font-bold sm:text-3xl text-center">Welcome {userJSON.username} </h1>
+                <h1 className="text-2xl font-bold sm:text-3xl text-center">
+                    Welcome {userJSON.username}
+                </h1>
                 <p className="mt-4 text-gray-500">
                     Welcome to the homepage of this simple full-stack authentication app built with Postgres, Prisma, Express, React, and TailwindCSS.
                 </p>
